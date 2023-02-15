4254a5819538e0f9a713fbb1a1c565f036e680c5
donghee <jjj4435@naver.com>
2023-02-15 14:05:32 +0900

    Mapper 인터페이스 구현

--- src/main/java/com/codestates/member/Member.java
+++ src/main/java/com/codestates/member/Member.java
@@ -2,10 +2,15 @@ package com.codestates.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class Member {
+    private long memberId;
     String email;
 
     String name;
--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -7,45 +7,74 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.codestates.member.mapstruct.mapper.MemberMapper;
 
 
 @RestController
-@RequestMapping(value = "/v1/members")
+@RequestMapping(value = "/v5/members")
 @Validated
 public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+    public MemberController(MemberService memberService, MemberMapper mapper){
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
     @PostMapping
-    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
-        System.out.println("# email: " + memberPostDto.getEmail());
-        System.out.println("# name: " + memberPostDto.getName());
-        System.out.println("# phone: " + memberPostDto.getPhone());
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberDto) {
+
+        Member member = mapper.memberPostDtoToMember(memberDto);
+        Member response = memberService.createMember(member);
+
+        System.out.println("# email: " + memberDto.getEmail());
+        System.out.println("# name: " + memberDto.getName());
+        System.out.println("# phone: " + memberDto.getPhone());
 
 
-        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.CREATED);
     }
     @PatchMapping("/{member-id}")
     public ResponseEntity patchMember(@PathVariable("member-id") @Min(2) long memberId,
                                       @Valid @RequestBody MemberPatchDto memberPatchDto){
         memberPatchDto.setMemberId(memberId);
 
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member response = memberService.updateMember(mapper.memberPatchDtoToMember(memberPatchDto));
+
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id")long memberId){
         System.out.println("# memberID: " + memberId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Member response = memberService.findMember(memberId);
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getMembers(){
         System.out.println("# get Members");
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<Member> members = memberService.findMembers();
+
+        List<MemberResponseDto> response =
+                members.stream()
+                        .map(member -> mapper.memberToMemberResponseDto(member))
+                        .collect(Collectors.toList());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId){
+
+        System.out.println("#delete member");
+
+        memberService.deleteMember(memberId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
